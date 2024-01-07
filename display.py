91308fcf9a1423874dea1a880fc9736f065b1e4a
nsd8888 <nileshdalagade88@gmail.com>
2024-01-07 17:51:25 +0530

    initial commit 6

---
+++ display.py
@@ -0,0 +1,4 @@
+with open("new.txt", "r") as t:
+    var=t.read()
+
+print(var)
--- script.py
+++ script.py
@@ -2,4 +2,6 @@ lst=[3,10,1,20,7,15,25]
 
 newlst = sorted(range(len(lst)), key= lambda k: lst[k])
 
-return newlst
+
+with open("new.txt", "w+") as t:
+    t.write("i am king of the hastinampur")
